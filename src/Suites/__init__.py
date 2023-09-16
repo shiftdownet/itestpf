560d5e293d8f8b93cb17dce3f660b82546b01d7e
Hiroki Nakayama <k11096kk@gmail.com>
2023-09-16 23:13:41 +0900

    updated.

--- src/Suites/__init__.py
+++ src/Suites/__init__.py
@@ -1,6 +1,10 @@
+import importlib
 import os
 for module in os.listdir(os.path.dirname(__file__)):
     if module == '__init__.py' or module[-3:] != '.py':
         continue
-    __import__("Suites." + module[:-3], locals(), globals())
+    module_name = module[:-3]
+    module = importlib.import_module("Suites." + module_name)
+    exec(module_name + " = getattr(module, module_name)")
+    #__import__("Suites." + module[:-3], locals(), globals())
 del module
--- src/TestProject_A.py
+++ src/TestProject_A.py
@@ -1,5 +1,6 @@
 
 
+import datetime
 from collections.abc import Iterator
 from Framework.ITestProject import ITestProject
 from Framework.TestDriver import TestDriver
@@ -15,18 +16,25 @@ from Suites import *
 class TestProject(ITestProject):
     def logger(self) -> ITestLogger:
         return YamlTestLogger()
+        # 以下も選択可能
+        #return XmlTestLogger()
+        #return TextTestLogger()
 
     def log_filename(self) -> str:
-        return "./test_log.yaml"
+        return "./log_latest.yaml"
+        # ログファイルに日付を含める場合は以下
+        #return './log_' + (datetime.datetime.now()).strftime('%Y%m%d_%H%M%S') + '.yaml'
     
     def log_encoding(self) -> str:
         return 'utf-8'
     
     def test_system(self) -> ITestSystem:
         return MockSimulator()
+        # 本番では以下を選択
+        #return CsPlusSimulator()
 
     def suites(self) -> Iterator:
-        yield Suite_Sample1.Suite_Sample1()
+        yield Suite_Sample1()
 
 if __name__ == "__main__": 
     TestDriver().launch( TestProject() )
